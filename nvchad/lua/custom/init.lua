a28b4250c250917c19227e737b4581f83323e915
dkf <dannzor@outlook.com>
2024-03-16 23:59:51 +0000

    add solidity formatting

--- nvchad/lua/custom/init.lua
+++ nvchad/lua/custom/init.lua
@@ -1,3 +1,16 @@
 vim.g.dap_virtual_text = true
 vim.g.rustfmt_autosave = 1
 
+-- Solidity indent
+vim.api.nvim_exec([[
+        augroup SolidityIndent
+            autocmd FileType solidity setlocal shiftwidth=4
+        augroup END;
+    ]], true)
+-- Solidity strip two or more blank lines
+vim.api.nvim_exec([[
+        augroup SolidityNewLine
+              autocmd BufWritePre *.sol if search('\n\n\{2,}', 'nw') | %s/\n\n\{2,}/\r\r/g | endif
+        augroup END;
+]], true)
+
